547962337231eefb392d2eed2fc7ccbd9a95ef02
Vinicius Smadeski <vinicius.js94@gmail.com>
2018-10-25 14:15:06 -0300

    Add contato success message

--- public/js/app.js
+++ public/js/app.js
@@ -16,3 +16,15 @@ for(element of typeds){
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', '/js/particles.json', function() {
 });
+
+$("form").submit( (event) => {
+    event.preventDefault();
+    Swal({
+        title: "Sucesso!",
+        text: "Mensagem enviada. Obrigado pelo seu contato!",
+        type: "success"
+    }).then(() => {
+        $("form").off("submit").submit();
+    });
+});
+
