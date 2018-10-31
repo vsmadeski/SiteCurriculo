9c56958e95a1f65e9630866a8c85be31d8035b78
Vinicius Smadeski <vinicius.js94@gmail.com>
2018-10-31 13:00:48 -0300

    Remove photo and reorganize home

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,6 @@ let osOptions = ["Android", "Windows Phone", "iOS"];
 let device = platform.os.family;
 
 if(!osOptions.includes(device)){
-    /*
-    let typeds = $(".typer-class");
-
-    let options = {
-        showCursor: false,
-        strings: ["", "Put final text here"],
-        typeSpeed: 10
-    }
-    
-    for(element of typeds){
-        let dummy = element.innerHTML;
-        element.innerHTML = "";
-        options.strings[1] = dummy;
-        new Typed(element, options);
-    }*/
-
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */ 
     particlesJS.load('particles-js', '/js/particles.json', function() {
     });
